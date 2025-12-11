7264c2d0c75019701c37d21ceeb60b016c1053ee
hameed <shahameed68@gmail.com>
2025-12-11 22:05:28 +0530

    feat: add business dashboard DTOs

---
+++ Models/Business/Dtos/BusinessDashboardDto.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Queueless.Models.Business.Dtos
+{
+    public class BusinessDashboardDto
+    {
+        public int BusinessId { get; set; }
+        public string BusinessName { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+        public string Area { get; set; } = string.Empty;
+
+        // from BusinessRegistration.AvgTimeMinutes
+        public int? AvgWaitMinutes { get; set; }
+
+        // current serving token number (can be null)
+        public string? CurrentTokenNumber { get; set; }
+
+        // how many are currently waiting
+        public int WaitingCount { get; set; }
+
+        // list of tokens in the waiting queue
+        public List<BusinessQueueItemDto> Queue { get; set; } = new();
+    }
+}
---
+++ Models/Business/Dtos/BusinessQueueItemDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Queueless.Models.Business.Dtos
+{
+    public class BusinessQueueItemDto
+    {
+        public int TokenId { get; set; }
+
+        // we convert numeric token to string in controller
+        public string TokenNumber { get; set; } = string.Empty;
+
+        public string CustomerName { get; set; } = string.Empty;
+
+        // for now not used (no Service table yet), but kept for future
+        public string ServiceName { get; set; } = string.Empty;
+    }
+}
